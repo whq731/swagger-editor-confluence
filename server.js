70923fe6f7f0403d9348fe3b59bbcec41acecb0f
weihanqing <weihanqing@meituan.com>
2016-08-19 17:21:58 +0800

    add default port 80

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ function startServer(port, cb) {
 
 // if this file was triggered directly, launch the server
 if (process.argv[1] === __filename) {
-  var PORT = process.env.PORT || 8080;
+  var PORT = process.env.PORT || 80;
 
   startServer(PORT, function(err) {
     if (err) {
